607e518e4acc9351d0387fc543cd7d2c8f07e827
marcelamoraless <114291545+marcelamoraless@users.noreply.github.com>
2025-02-20 16:47:44 -0300

    Update formulario.cy.js

--- cypress/e2e/Testes Narwal/formulario.cy.js
+++ cypress/e2e/Testes Narwal/formulario.cy.js
@@ -13,13 +13,13 @@ Given que acessei a página principal do site "Narwal Sistemas"
 When eu clico no botão "Solicite uma demonstração"
 Then eu devo acessar a página do site Narwal "/contato"
 
-When eu preencho o campo "Nome" com "Marcela Morales"
-and eu preencho o campo "Email" com "marceladefranceschi@gmail.com"
-and eu preencho o campo "Cargo" com "Analista de teste"
-and eu preencho o campo "Telefone" com "(48)991854484"
-and eu preencho o campo "Empresa" com "Narwal Sistemas"
-and eu seleciono no campo "Qual segmento" com a opção "3"
-Then eu não devo submeter o formulario (evitando enviar informações, sempre enviar para testes)
+When o usuário preenche o campo "Nome" com "Marcela Morales"
+and o usuário preenche o campo "Email" com "marceladefranceschi@gmail.com"
+and o usuário preenche o campo "Cargo" com "Analista de teste"
+and o usuário preenche o campo "Telefone" com "(48)991854484"
+and o usuário preenche campo "Empresa" com "Narwal Sistemas"
+and o usuário seleciona no campo "Qual segmento" com a opção "3"
+Then o usuário não deve submeter o formulario (evitando enviar informações, sempre enviar para testes)
 and o formulário deve permanecer na página
 
 Não realizei o envio para não estar enviando formularios mas num ambiente interno de teste estaria realizando normalmente
@@ -27,6 +27,13 @@ Scenario: Enviar o formulário com sucesso
 Given o usuário está na página do formulário de contato
 When o usuário clica no botão "Enviar" com os campos obrigatórios preenchidos
 Then o sistema deve exibir mensagens de sucesso
+
+Scenario: Preencher o email com um email invalido
+Given o usuário está na página do formulário de contato
+When o usuário preenche todos os campos com as informações anteriores
+and o usuário preenche o "Email" com "teste@teste"
+and o usuario clica no botão "Enviar" 
+Then o sistema deve acusar erro no campo "Email"
 */
 
 describe('Teste de preenchimento do formulário para contato', () => {
@@ -45,4 +52,22 @@ describe('Teste de preenchimento do formulário para contato', () => {
         cy.get('#rd-select_field-m4l7psxn').select(3)
 
     });
+
+    it('Preencher o formulario com o email invalido', () => {
+        cy.visit('https://www.narwalsistemas.com.br/')
+        cy.viewport(1280, 800)
+
+        cy.contains('SOLICITE UMA DEMONSTRAÇÃO').click()
+        cy.url().should('include', '/contato')
+
+        cy.get('#rd-text_field-m4l7psxi').type('Marcela Morales')
+        cy.get('#rd-email_field-m4l7psxj').type('teste@teste') //email invalido
+        cy.get('#rd-text_field-m4l7psxk').type('Analista de teste')
+        cy.get('#rd-phone_field-m4l7psxl').type('(48)991854484')
+        cy.get('#rd-text_field-m4l7psxm').type('Narwal Sistemas')
+        cy.get('#rd-select_field-m4l7psxn').select(3)
+        cy.get('#rd-button-m4ikgof1').click()
+        cy.get('#rd-email_field-m4l7psxj').should('have.css', 'border-color', 'rgb(255, 0, 0)')
+
+    })
 });
